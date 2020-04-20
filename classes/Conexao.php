2e354d5716077333db96a866924451b498d1b3cd
Thiago <thiago.sousa78@homail.com>
2020-04-20 19:25:23 -0300

    select e delete

--- classes/Conexao.php
+++ classes/Conexao.php
@@ -10,7 +10,7 @@ class Conexao
 	public function __construct() {
 		$this->host = 'mysql:host=localhost;dbname=lanchonete_api';
 		$this->user = 'root';
-		$this->password = 'root';
+		$this->password = '12345';
 	}
 
     public function conectar() {
